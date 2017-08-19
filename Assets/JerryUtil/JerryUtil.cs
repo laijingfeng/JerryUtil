662dcc295cf7290b2a07ca8b910ce43c0de66b5d
laijingfeng <337292616@qq.com>
2017-08-19 20:36:10 +0800

    update

--- Assets/JerryUtil/JerryUtil.cs
+++ Assets/JerryUtil/JerryUtil.cs
@@ -427,22 +427,14 @@ namespace Jerry
         public static Vector3 GetClickPos()
         {
             Vector3 pos = Input.mousePosition;
-#if UNITY_EDITOR
-            pos = Input.mousePosition;
-#else
-#if UNITY_ANDROID || UNITY_IPHONE
-            if(Input.touchCount > 0)
+            if(Application.platform == RuntimePlatform.Android
+                || Application.platform == RuntimePlatform.IPhonePlayer)
             {
-                pos = Input.touches[0].position;
+                if (Input.touchCount > 0)
+                {
+                    pos = Input.touches[0].position;
+                }
             }
-            else
-            {
-                pos = Input.mousePosition;
-            }
-#else
-            pos = Input.mousePosition;
-#endif
-#endif
             pos.z = 0;
             return pos;
         }
