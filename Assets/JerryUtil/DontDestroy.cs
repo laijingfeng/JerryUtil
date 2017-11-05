92d271e25ddf6452c9c51b5b5204054009b7b101
laijingfeng <337292616@qq.com>
2017-11-05 18:33:36 +0800

    update

---
+++ Assets/JerryUtil/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Jerry
+{
+    public class DontDestroy : MonoBehaviour
+    {
+        void Awake()
+        {
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
