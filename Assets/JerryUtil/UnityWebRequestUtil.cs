697109ac4e647f1a2e015ba6906dfb4866e1d366
laijingfeng <337292616@qq.com>
2017-11-08 20:14:33 +0800

    update

---
+++ Assets/JerryUtil/UnityWebRequestUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine.Experimental.Networking;
+
+namespace Jerry
+{
+    public class UnityWebRequestUtil : Singleton<UnityWebRequestUtil>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="callback">code-rsp-err</param>
+        public void Get(string url, Action<long, string, string> callback)
+        {
+            new JerryCoroutine.CorTask(IE_Get(url, callback));
+        }
+
+        private IEnumerator IE_Get(string url, Action<long, string, string> callback)
+        {
+            using (UnityWebRequest r = UnityWebRequest.Get(url))
+            {
+                yield return r.Send();
+                if (r.isError)
+                {
+                    if (callback != null)
+                    {
+                        callback(r.responseCode, null, r.error);
+                    }
+                }
+                else
+                {
+                    if (callback != null)
+                    {
+                        callback(r.responseCode, r.downloadHandler.text, null);
+                    }
+                }
+            }
+        }
+    }
+}
